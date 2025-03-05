7d15fabaaf3408c4fc78c40893152b7675fecdf2
Koval89 <alexandrgalaxy4@gmail.comsdfsdfsdgdssgsdgdgds>
2025-03-05 21:46:11 +0200

    HW-1

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const fs = require('node:fs/promises')
+const fs = require('node:fs')
 const path = require('node:path')
+const readline = require('node:readline/promises')
 
-// const file = path.join(process.cwd(),'emails.txt')
 
-const start = async () => {
-    const filePath = path.join(process.cwd(), 'emails.txt');
-    // await fs.open('gmail.txt', 'w');
-   await fs.readFile(filePath,{encoding:'utf8'})
+ // fs.open('gmail.txt', 'w');
 
-}
+// const readStream = fs.createReadStream('emails.txt');
+// const writeStream = fs.createWriteStream('gmail.txt');
+//
+// const rl = readline.createInterface({
+//     input: readStream
+// })
+// rl.on('line',(line)=>{
+//     if (line.includes('gmail.com')){
+//         writeStream.write(line+'\n')
+//     }
+// })
+// rl.on('close',()=>{
+//     writeStream.end()
+// })
 
-start();
+fs.readFile('gmail.txt',{encoding:"utf-8"},(err,data)=>{
+    if(err) throw err
+    const fileOnlyGmail = data
+        .split('\n')
+        .map(line=>line.split(/\s+/).pop())
+        .join('\n');
+    fs.writeFile('onlyGmail.txt',fileOnlyGmail,(err)=>{
+        if(err) throw err
+    })
+})
